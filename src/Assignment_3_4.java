84ea2d4683cc54fcfc3498156b0ee596d8ceba4f
754322 <754322@121-PC17.student.Tor.Loc>
2019-07-15 17:42:32 -0400

    Added New Class

--- src/Assignment_3_4.java
+++ src/Assignment_3_4.java
@@ -1,5 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Assignment_3_4 {
+public class Assignment_3_4 extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Define UI Control Elements
+		
+		
+		//Defining Layout Manager
+		VBox root = new VBox();
+		
+		//adding UI Control Elements inside Layout Manager
+		
+		
+		//Creating new Scene
+		Scene scene = new Scene(root, 400,600);
+		// Setting Scene inside Stage
+		primaryStage.setScene(scene);
+	}
+	
+	
 	
 
 
